2689459631f030616ee7d363045c206e67db400d
KishoreGanesh411 <kishoreganeshk14@gmail.com>
2025-08-05 15:20:11 +0530

    Initial commit

---
+++ apps/webApp-crm/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@ipk-wealth/webApp-crm" />
+    </div>
+  );
+}
+
+export default App;
